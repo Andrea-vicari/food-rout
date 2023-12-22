bbb64675013cb8c491a36fb67580d1b4d453ba8e
Andrea-vicari <andrea.vicari77@gmail.com>
2023-12-22 09:06:23 +0100

    OK fixed some links

--- src/Components/Common/header.jsx
+++ src/Components/Common/header.jsx
@@ -9,14 +9,14 @@ function Header() {
     <Link className="navbar-brand" to="/">
     <img src={logo} width='120'></img>
     </Link>
-    <button className="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+    <button className="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="true" aria-label="Toggle navigation">
         <i className='fa fa-bars fs-3 text-black'></i>
     </button>
     <div className="collapse navbar-collapse" id="navbarCollapse">
       <ul className="navbar-nav col-lg-10  d-flex justify-content-around align-content-end">
         <li className="nav-item fs-5 d-flex align-items-center">
           <i className='fa fa-home d-none d-sm-block'></i>
-            <Link className="nav-link active" to="/">Home</Link>
+            <Link className="nav-link" to="/" >Home</Link>
         </li>
         <li className="nav-item fs-5 d-flex align-items-center">
           <i className='fa fa-book d-none d-sm-block'></i>
@@ -28,7 +28,7 @@ function Header() {
         </li>
 
       </ul>
-      <Link to="/FavRecList" className="text-decoration-none">
+      <Link to="/FavRecList" className="text-decoration-none" data-bs-toggle="collapse">
       <i className="px-2 text-black fa fa-heart fs-5"></i>
       </Link>
     </div>
